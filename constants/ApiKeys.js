2ff5babfed501558fc55fe3a1c13df22df55580e
ZakBrinlee <zbrinlee@hotmail.com>
2018-10-07 09:29:35 -0700

    New files from Firebase Authentication working

---
+++ constants/ApiKeys.js
@@ -0,0 +1,10 @@
+export default {
+	FirebaseConfig: {
+		apiKey: "AIzaSyArGaNYlK-TGwJePyDBfv2f621MSev7cyQ",
+    authDomain: "project-8-privyd.firebaseapp.com",
+    databaseURL: "https://project-8-privyd.firebaseio.com",
+    projectId: "project-8-privyd",
+    storageBucket: "project-8-privyd.appspot.com",
+    messagingSenderId: "7975847010"
+	}
+}
