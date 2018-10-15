862746ee1a2cf5519e8d0bad9d8e068a9b82d427
DM Coulter <dcoult01@seattlecentral.edu>
2018-10-14 21:10:14 -0700

    Revert "Changes to Signup for issue 15"

--- screens/auth/SignupScreen.js
+++ screens/auth/SignupScreen.js
@@ -4,7 +4,6 @@ import { StyleSheet, View, Text, TextInput, Button, Alert } from 'react-native';
 import { NavigationActions } from 'react-navigation';
 import * as firebase from 'firebase';
 import SwitchSelector from 'react-native-switch-selector';//for toggle selector
-import ApiKeys from '../../constants/ApiKeys.js';
 
 //Used this tutorial for toggle switch https://github.com/App2Sales/react-native-switch-selector
 
@@ -17,7 +16,6 @@ export default class SignupScreen extends React.Component {
             email: "",
             password: "",
             passwordConfirm: "",
-            role: "client",
         };
     }
 
@@ -29,33 +27,7 @@ export default class SignupScreen extends React.Component {
 
         firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
             .then(() => { }, (error) => { Alert.alert(error.message); });
-
-        this.writeUserData(this.state.email, this.state.role);
-        // firebase.database().ref('UsersList/').push({
-        //     this.state.email,
-        //     this.state.role
-        // }).then((data)=>{
-        //     //success callback
-        //     console.log('data ' , data)
-        // }).catch((error)=>{
-        //     //error callback
-        //     console.log('error ' , error)
-        // })
-        
     }
-
-    writeUserData(email = this.state.email, role = this.state.role){
-        firebase.database().ref('UsersList/').push({
-            email,
-            role
-        }).then((data)=>{
-            //success callback
-            console.log('data ' , data)
-        }).catch((error)=>{
-            //error callback
-            console.log('error ' , error)
-        })
-    };
 
     onBackToLoginPress = () => {
         var navActions = NavigationActions.reset({
@@ -103,7 +75,8 @@ export default class SignupScreen extends React.Component {
                 />
                 
                 <View style={{paddingTop:30}} />
-        
+
+                //toggle        
                 <SwitchSelector
                     initial={0}
                     onPress={value => this.setState({ role: value })}
@@ -111,7 +84,7 @@ export default class SignupScreen extends React.Component {
                     selectedColor={'#1a1a1a'}
                     buttonColor={'#99ffcc'}
                     borderColor={'#cc99ff'}
-                    fontSize={20}
+                    fontSize={'20px'}
                     options={options}
 
                    />
